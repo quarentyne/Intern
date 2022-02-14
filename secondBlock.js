69817db2626fafe378d22a02cdbe322ac188fb1c
quarentyne <peresada1997@gmail.com>
2022-02-14 10:57:41 +0200

    Call and bind can work in prototypes

--- secondBlock.js
+++ secondBlock.js
@@ -1,22 +1,30 @@
 'use strict';
 
-function myBind(foo, context, ...rest) {
+Function.prototype.myBind = function (context, ...rest) {
+  if (typeof context !== 'object' || typeof context !== 'function') {
+    return function boundFunc() {
+    };
+  }
+  const there = this;
+  const insideObject = Object.create(context);
   return function (...args) {
     const uniqElement = Symbol();
-    context[uniqElement] = foo;
-    const result = context[uniqElement](...rest, ...args);
-    delete context[uniqElement];
+    insideObject[uniqElement] = there;
+    const result = insideObject[uniqElement](...rest, ...args);
+    delete insideObject[uniqElement];
+    console.log(result);
     return result;
-  }    
-}
+  }
+};
 
-function myCall(foo, context, ...args) {
+Function.prototype.myCall = function (context, ...args) {
+  const insideObject = Object.create(context);
   const uniqElement = Symbol();
-  context[uniqElement] = foo;
-  const result = context[uniqElement](...args);
-  delete context[uniqElement];
+  insideObject[uniqElement] = this;
+  const result = insideObject[uniqElement](...args);
+  delete insideObject[uniqElement];
   return result;
-}
+};
 
 Array.prototype.myMap = function (callback) {
   if (typeof callback !== "function") {
@@ -27,20 +35,20 @@ Array.prototype.myMap = function (callback) {
     result.push(callback(this[i], i, this));
   }
   return result;
-}
+};
 
 Array.prototype.myFilter = function (callback) {
   if (typeof callback !== "function") {
     throw new Error('Callback have to be Function type');
   }
   const result = [];
-  for (let i = 0; i < this.length; i++){
+  for (let i = 0; i < this.length; i++) {
     if (callback(this[i], i, this)) {
       result.push(this[i])
     }
   }
   return result;
-}
+};
 
 Array.prototype.myReduce = function (callback, initialValue) {
   if (typeof callback !== "function") {
@@ -50,32 +58,32 @@ Array.prototype.myReduce = function (callback, initialValue) {
     throw new Error('reduce of empty array with no initial value')
   }
   let result = initialValue || 0;
-  for (let i = 0; i < this.length; i++){
+  for (let i = 0; i < this.length; i++) {
     result = callback(result, this[i], i, this);
   }
   return result;
-}
+};
 
 Array.prototype.myFind = function (callback) {
   if (typeof callback !== "function") {
     throw new Error('Callback have to be Function type');
   }
-  for (let i = 0; i < this.length; i++){
+  for (let i = 0; i < this.length; i++) {
     if (callback(this[i], i, this)) {
       return this[i];
     }
   }
   return undefined;
-}
+};
 
 Array.prototype.myForEach = function (callback) {
   if (typeof callback !== "function") {
     throw new Error('Callback have to be Function type');
   }
-  for (let i = 0; i < this.length; i++){
+  for (let i = 0; i < this.length; i++) {
     if (!this[i]) {
       continue;
     }
     callback(this[i], i, this);
   }
-}
+};
